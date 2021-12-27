0be581deb46bfe0b01cd171de4ad3ef8ef6bf4b6
Kajus-Padelskas <kajus.padelskas@gmail.com>
2021-12-27 20:22:06 +0200

    Setted up a class for GUI renderer

---
+++ Snake/Renderer/GUIRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Renderer
+{
+    class GUIRenderer : IRenderer
+    {
+        private readonly Snake _snake;
+        private readonly Apple _apple;
+        private readonly Board _gameBoard;
+        public GUIRenderer(Board map, Snake snake, Apple apple)
+        {
+            this._gameBoard = map;
+            this._snake = snake;
+            this._apple = apple;
+        }
+
+        public void Render()
+        {
+            
+        }
+    }
+}

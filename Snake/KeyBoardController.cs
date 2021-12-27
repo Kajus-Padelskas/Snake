5e4a42eadeebfb886208b8b5512e913c0bfeef35
Kajus-Padelskas <kajus.padelskas@gmail.com>
2021-12-27 16:57:12 +0200

    Added Thread which listens for user input while game has not ended

---
+++ Snake/KeyBoardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class KeyBoardController
+    {
+        public Thread KeyboardThread { get; set; }
+
+        public KeyBoardController(SnakeGame game)
+        {
+            KeyboardThread = new Thread(() =>
+            {
+                while (game.IsGameOver)
+                {
+                    var userInput = Console.ReadKey();
+                    Console.WriteLine();
+                    game.ProcessUserCommand(userInput);
+                }
+            });
+        }
+    }
+}

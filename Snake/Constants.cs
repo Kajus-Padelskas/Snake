5d5482ead511c37e19ef1294c1f82d2a75294537
Kajus-Padelskas <kajus.padelskas@gmail.com>
2022-01-04 00:35:43 +0200

    Added more constants for fixing magic numbers

--- Snake/Constants.cs
+++ Snake/Constants.cs
@@ -4,7 +4,10 @@
     {
         public const int SNAKE_START_Y_POS = 0;
         public const int SNAKE_START_X_POS = 0;
-        public const int TIME_OUT_FOR_USER_INPUT = 500;
         public const int TIME_OUT_FOR_RENDERING = 500;
+        public const int SNAKE_HEAD = 2;
+        public const int SNAKE_BODY = 4;
+        public const int SNAKE_TAIL = 3;
+        public const int APPLE = 5;
     }
 }
